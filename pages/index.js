2f23add673d03113b5d320ba51cc0d103f5f96b4
bigballaband <122322057+bigballaband@users.noreply.github.com>
2023-06-27 23:16:55 -0400

    attempt 10000000

--- pages/index.js
+++ pages/index.js
@@ -20,33 +20,66 @@ export async function getStaticProps() {
 export default function Home({ allPostsData }) {
   return (
     <Layout home>
+      <style type="text/css" media="screen">
+        {`
+        body {
+          margin: 0;
+          padding: 0;
+        }
+
+        div#banner {
+          position: absolute;
+          top: 0;
+          left: 0;
+          background-color: #DDEEEE;
+          width: 100%;
+        }
+
+        div#banner-content {
+          width: 800px;
+          margin: 0 auto;
+          padding: 10px;
+          border: 1px solid #000;
+        }
+
+        div#main-content {
+          padding-top: 70px;
+        }
+        `}
+      </style>
       <Head>
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>Hello, my name is Thomas Thangarajah and I am a second year university student at the University of Waterloo. I am currently in the 
-          Math program. You can find my links and personal projects below!</p>
+        <p>
+          Hello, my name is Thomas Thangarajah and I am a second-year university student at the University of Waterloo. I am currently in the Math program. You can find my links and personal projects below!
+        </p>
       </section>
-      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}> 
-      <h2 className={utilStyles.headingLg}>Links: </h2>
-      <h5><a href="https://www.linkedin.com/in/thomas-thangarajah-25b690208/">Linkedln</a><br></br>
-      <a href="https://github.com/bigballaband">Github</a><br></br>
-      Instagram: @darealbigballaband</h5>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Links:</h2>
+        <h5>
+          <a href="https://www.linkedin.com/in/thomas-thangarajah-25b690208/">Linkedln</a>
+          <br />
+          <a href="https://github.com/bigballaband">Github</a>
+          <br />
+          <a href="https://www.instagram.com/darealbigballaband/">Instagram</a>
+        </h5>
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Personal Projects:</h2>
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }) => (
             <li className={utilStyles.listItem} key={id}>
-            <Link href={`/posts/${id}`}>{title}</Link>
-            <br />
-            <small className={utilStyles.lightText}>
-              <Date dateString={date} />
-            </small>
-          </li>
+              <Link href={`/posts/${id}`}>{title}</Link>
+              <br />
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
+            </li>
           ))}
         </ul>
       </section>
     </Layout>
   );
 }
+
