bba22fb3634e7e0d7e2d25068ab027648e161b1a
bigballaband <122322057+bigballaband@users.noreply.github.com>
2023-06-27 22:37:47 -0400

    change

--- components/layout.js
+++ components/layout.js
@@ -5,7 +5,7 @@ import utilStyles from '../styles/utils.module.css';
 import Link from 'next/link';
 
 const name = 'Thomas Thangarajah';
-export const siteTitle = 'Next.js Sample Website';
+export const siteTitle = 'Thomas Thangarajah\'s website';
 
 export default function Layout({ children, home }) {
   return (
@@ -14,7 +14,7 @@ export default function Layout({ children, home }) {
         <link rel="icon" href="/favicon.ico" />
         <meta
           name="description"
-          content="Learn how to build a personal website using Next.js"
+          content="Personal Website"
         />
         <meta
           property="og:image"
