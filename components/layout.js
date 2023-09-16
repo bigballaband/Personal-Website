7ee98606c6cff552558f7f3b9af5a71a21b2a1c2
bigballaband <122322057+bigballaband@users.noreply.github.com>
2023-09-16 18:35:45 -0400

    attempting to change background

--- components/layout.js
+++ components/layout.js
@@ -3,6 +3,7 @@ import Image from 'next/image';
 import styles from './layout.module.css';
 import utilStyles from '../styles/utils.module.css';
 import Link from 'next/link';
+import '../styles/global.css';
 
 const name = 'Thomas Thangarajah';
 export const siteTitle = 'Thomas Thangarajah\'s website';
